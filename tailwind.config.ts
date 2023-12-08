e1faafb4fa9e803892158ec81c95b119df44fdb3
Jihun Song <songjihun940409@gmail.com>
2023-12-08 14:47:52 +0900

    초기 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        hel: ["Helvetica Neue"],
+      },
+      colors: {
+        default: {
+          100: "#FBFAF2",
+          200: "#F5F3EB",
+          300: "#EBE3D5",
+          400: "#DED0B6",
+          500: "#143422",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;

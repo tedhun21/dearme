45f4d1456c02534c1cbb64da9faa130535ab8887
Givehim <manndu125@gmail.com>
2024-02-27 10:43:34 +0900

    [FE]Fix: diary, Companion 중복 선택에서 단일 선택으로 변경 (#13)

--- app/ui/diary/ChooseCompanions.tsx
+++ app/ui/diary/ChooseCompanions.tsx
@@ -7,18 +7,17 @@ export default function ChooseCompanions({
 }: {
   onSelectCompanion: (companion: string[]) => void;
 }) {
-  const [selectedCompanions, setSelectedCompanions] = useState([] as string[]);
+  const [selectedCompanions, setSelectedCompanions] = useState<string | null>(
+    null,
+  );
   const companions = ["가족", "연인", "친구", "지인", "안만남"];
 
-  const handleCompanionClick = (companion: any) => {
-    setSelectedCompanions((prevCompanions) => {
-      // 이미 선택된 동반자라면 제거, 아니라면 추가
-      const newCompanions = prevCompanions.includes(companion)
-        ? prevCompanions.filter((c) => c !== companion)
-        : [...prevCompanions, companion];
-      onSelectCompanion(newCompanions); // 부모 컴포넌트에 선택된 동반자들을 알림
-      return newCompanions;
-    });
+  const handleCompanionClick = (companion: string) => {
+    // 이미 선택된 동반자를 클릭한 경우 선택 해제, 아니면 선택
+    const newSelectedCompanion =
+      selectedCompanions === companion ? null : companion;
+    setSelectedCompanions(newSelectedCompanion);
+    onSelectCompanion(newSelectedCompanion ? [newSelectedCompanion] : []);
   };
 
   return (
@@ -27,7 +26,7 @@ export default function ChooseCompanions({
         <Companions
           key={companion}
           text={companion}
-          selected={selectedCompanions.includes(companion)}
+          selected={selectedCompanions === companion}
           onClick={() => handleCompanionClick(companion)}
         />
       ))}
